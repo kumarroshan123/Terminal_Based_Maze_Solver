6974ef48e3d968a63f32da0c95900b51955585f0
kumarroshan123 <kumar30roshan@gmail.com>
2024-01-17 10:35:23 +0530

    Random_Maze_Generator

--- Rat_in_maze.py
+++ Rat_in_maze.py
@@ -28,5 +28,67 @@ class Stack:
 
     def top(self):
         return self.head.val
+    def random_maze_generator(r, c, P0, Pf):
+        ROWS, COLS = r, c
+        maze = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+        seen = [[False for _ in range(COLS)] for _ in range(ROWS)]
+        previous = [[(-1, -1) for _ in range(COLS)] for _ in range(ROWS)]
+
+        S = Stack()
+        S.insert(P0)
+
+        while S.not_empty():
+            x, y = S.pop()
+            seen[x][y] = True
+
+            if (x + 1 < ROWS) and maze[x + 1][y] == 1 and previous[x][y] != (x + 1, y):
+                continue
+            if (0 < x) and maze[x-1][y] == 1 and previous[x][y] != (x-1, y):
+                continue
+            if (y + 1 < COLS) and maze[x][y + 1] == 1 and previous[x][y] != (x, y + 1):
+                continue
+            if (y > 0) and maze[x][y-1] == 1 and previous[x][y] != (x, y-1):
+                continue
+
+            maze[x][y] = 1
+            to_stack = []
+
+            if (x + 1 < ROWS) and not seen[x + 1][y]:
+                seen[x + 1][y] = True
+                to_stack.append((x + 1, y))
+                previous[x + 1][y] = (x, y)
+
+            if (0 < x) and not seen[x-1][y]:
+                seen[x-1][y] = True
+                to_stack.append((x-1, y))
+                previous[x-1][y] = (x, y)
+
+            if (y + 1 < COLS) and not seen[x][y + 1]:
+                seen[x][y + 1] = True
+                to_stack.append((x, y + 1))
+                previous[x][y + 1] = (x, y)
+
+            if (y > 0) and not seen[x][y-1]:
+                seen[x][y-1] = True
+                to_stack.append((x, y-1))
+                previous[x][y-1] = (x, y)
+
+            pf_flag = False
+            while to_stack:
+                neighbour = to_stack.pop(randint(0, len(to_stack) - 1))
+                if neighbour == Pf:
+                    pf_flag = True
+                else:
+                    S.insert(neighbour)
+
+            if pf_flag:
+                S.insert(Pf)
+
+        x0, y0 = P0
+        xf, yf = Pf
+        maze[x0][y0] = 2
+        maze[xf][yf] = 3
+
+        return maze
 
 
