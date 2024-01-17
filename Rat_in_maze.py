d93c4ccc083e0f2a424e2ad7138556d882f4a181
kumarroshan123 <kumar30roshan@gmail.com>
2024-01-17 22:46:45 +0530

    Maze_Path_Solution_Function_Fixed_Using_Backtracking

--- Rat_in_maze.py
+++ Rat_in_maze.py
@@ -28,8 +28,10 @@ class Stack:
 
     def top(self):
         return self.head.val
-
+maze=[] 
+path=[]
 def random_maze_generator(r, c, P0, Pf):
+    global maze
     ROWS, COLS = r, c
 
     maze = [[0 for _ in range(COLS)] for _ in range(ROWS)]
@@ -90,47 +92,75 @@ def random_maze_generator(r, c, P0, Pf):
     xf, yf = Pf
     maze[x0][y0] = 2
     maze[xf][yf] = 3
-
     return maze
+
 def display_maze_with_path(maze, path):
   symbols = {0: "▓", 1: "◌", 2: "S", 3: "E", -1: "◍"}  
   maze_copy = [row[:] for row in maze]
   for x, y in path:
       maze_copy[x][y] = -1
-
   for row in maze_copy:
       print(" ".join(symbols[cell] for cell in row))
-        
-def dfs(x, y, path, maze, Pf):
-    if x < 0 or x >= len(maze) or y < 0 or y >= len(maze[0]) or maze[x][y] != 1 or maze[x][y] == -1:
+
+def dfs(visited,curcord):
+    global maze,path
+    
+    if (not (curcord[0]>=0 and curcord[0]<len(maze) and curcord[1]>=0 and curcord[1]<len(maze))) or maze[curcord[0]][curcord[1]]==0:
         return False
-
-    path.append((x, y))
-    # print(path)
-    if (x, y) == Pf:
+    
+    if curcord[0]==len(maze)-1 and curcord[1]==len(maze)-1 :
         return True
 
-    maze[x][y] = -1
+    directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-    if (dfs(x + 1, y, path, maze, Pf) or
-        dfs(x - 1, y, path, maze, Pf) or
-        dfs(x, y + 1, path, maze, Pf) or
-        dfs(x, y - 1, path, maze, Pf)):
-        return True
-
-    maze[x][y] = 1
-    path.pop()
+    for direction in directions:
+        newCord = (curcord[0]+direction[0],curcord[1]+direction[1])
+        if (not (newCord[0]>=0 and newCord[0]<len(maze) and newCord[1]>=0 and newCord[1]<len(maze))) :
+            continue
+        if(not visited[newCord[0]][newCord[1]]):
+            visited[newCord[0]][newCord[1]]=True
+            flag = dfs(visited,newCord)
+            if flag :
+                path.append(newCord)
+                return True
+            else:
+                visited[newCord[0]][newCord[1]]= False
+                
     return False
+    
+    
+    
+        
+# def dfs(x, y, path, maze, Pf):
+#     if x < 0 or x >= len(maze) or y < 0 or y >= len(maze[0]) or maze[x][y] != 1 or maze[x][y] == -1:
+#         return False
 
-def find_path(maze, P0, Pf,path):
+#     path.append((x, y))
+#     # print(path)
+#     if (x, y) == Pf:
+#         return True
+
+#     maze[x][y] = -1
+
+#     if (dfs(x + 1, y, path, maze, Pf) or
+#         dfs(x - 1, y, path, maze, Pf) or
+#         dfs(x, y + 1, path, maze, Pf) or
+#         dfs(x, y - 1, path, maze, Pf)):
+#         return True
+
+#     maze[x][y] = 1
+#     path.pop()
+#     return False
+
+def find_path():
+    global maze
+    vis=[[False for _ in range(len(maze))] for _ in range(len(maze))]
     
-    
-    if dfs(0, 1, path, maze, Pf):
-        print(path)
-        return path 
-    if dfs(1, 0, path, maze, Pf):
-        print(path)
-        return path 
+    dfs(vis,(0,0))
+    #     return path 
+    # if dfs(1, 0, path, maze, Pf):
+    #     print(path)
+    #     return path 
 #   dfs(P0[0]+1, P0[1], path, maze, Pf)
     # print(path)
     # return path
@@ -139,10 +169,9 @@ if __name__ == "__main__":
     n = int(input("Enter the size of the maze (n x n): "))
     P0 = (0, 0)
     Pf = (n - 1, n - 1)
-    maze = random_maze_generator(n, n, P0, Pf)
-
+    maze=random_maze_generator(n, n, P0, Pf)
+    display_maze_with_path(maze, [])
     while True:
-        display_maze_with_path(maze, [])
         print("\nOptions:")
         print("1. Print the path")
         print("2. Generate another puzzle")
@@ -152,7 +181,7 @@ if __name__ == "__main__":
 
         if choice == "1":
             path=[]
-            find_path(maze, P0, Pf,path)
+            find_path()
             if len(path)>0:
                 display_maze_with_path(maze, path)
             else:
@@ -160,6 +189,7 @@ if __name__ == "__main__":
 
         elif choice == "2":
             maze = random_maze_generator(n, n, P0, Pf)
+            display_maze_with_path(maze, [])
 
         elif choice == "3":
             print("Exiting the game. Goodbye!")
